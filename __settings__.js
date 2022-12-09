654d3c070e4478f8a9717be19e70822a18c3d197
Elijah Newman-Gomez <smsithlord@yahoo.com>
2022-12-09 09:09:48 -0800

    Initial version.

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1611726.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 114510795, 114510803, 114510805, 114510819, 114510822, 114510829 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/114510788/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/114510789/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/114510787/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/114510792/1/basis.wasm.js', 'wasmUrl' : 'files/assets/114510793/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/114510791/1/basis.js', 'preload' : false},
+];
